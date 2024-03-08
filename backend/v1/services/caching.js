8b53a78a4d974d3bb64502d3eb389b9635bffd1b
mdekabs <1mawoda@gmail.com>
2024-03-08 11:32:09 +0100

    caching

--- backend/v1/services/caching.js
+++ backend/v1/services/caching.js
@@ -1,39 +1,29 @@
 
-import { redisClient } from '../../app.js';
+import { createRedisClient } from '../../redisClient.js'; // Adjust the import path as necessary
 import { logger } from '../middleware/logger.js';
 
 // Default constant for cache expiration time in seconds (20 days).
 const CACHE_EXPIRATION_TIME = 20 * 86400;
 
+// Initialize the Redis client at the module level
+let redisClient;
+(async () => {
+ redisClient = await createRedisClient(process.env.ENVIR);
+})();
+
 const redisManager = {
-  /**
-   * Deletes the cache data for a specific key in a hash.
-   *
-   * @param {string} hash - The hash name.
-   * @param {string} key - The key to delete.
-   * @return {undefined} Returns nothing.
-   */
-  cacheDel: async (hash, key) => {
+ cacheDel: async (hash, key) => {
     try {
       await redisClient.hDel(hash, key);
-
       logger.info(`${key} cache data deleted`);
       return;
     } catch (err) {
       logger.error(err);
       return;
     }
-  },
+ },
 
-  /**
-   * Sets a value in the cache with the given hash, key, and value.
-   *
-   * @param {string} hash - The hash to set the value in.
-   * @param {string} key - The key to set the value with.
-   * @param {any} value - The value to set in the cache.
-   * @return {undefined} Returns undefined.
-   */
-  cacheSet: async (hash, key, value) => {
+ cacheSet: async (hash, key, value) => {
     try {
       await Promise.all([
         redisClient.hSet(hash, key, value),
@@ -45,16 +35,9 @@ const redisManager = {
       logger.error(err);
       return;
     }
-  },
+ },
 
-  /**
-   * Retrieves the value associated with a given key in a hash stored in the cache.
-   *
-   * @param {string} hash - The name of the hash.
-   * @param {string} key - The key to retrieve the value for.
-   * @returns {Promise<string>} A Promise that resolves with the value associated with the key.
-   */
-  cacheGet: async (hash, key) => {
+ cacheGet: async (hash, key) => {
     try {
       const data = await redisClient.hGet(hash, key);
       return data;
@@ -62,7 +45,7 @@ const redisManager = {
       logger.error(err);
       return;
     }
-  }
+ }
 };
 
 export default redisManager;
