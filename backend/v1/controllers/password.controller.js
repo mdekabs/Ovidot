a30432a5a3acca3f7878cf755810b260e4942c33
mdekabs <1mawoda@gmail.com>
2024-03-13 14:09:20 +0100

    done

--- backend/v1/controllers/password.controller.js
+++ backend/v1/controllers/password.controller.js
@@ -1,7 +1,8 @@
+
 // PASSWORD CONTROLLER
 import dotenv from 'dotenv';
 import User from '../models/user.model.js';
-import { v4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import bcrypt from 'bcryptjs';
 import { handleResponse } from '../utility/handle.response.js';
 import { isTokenBlacklisted, updateBlacklist } from '../middleware/tokenBlacklist.js';
@@ -14,47 +15,37 @@ dotenv.config();
 
 const { genSalt, hash, compare } = bcrypt;
 
-/**
- * Default URL host for generating reset links.
- * @constant {string}
- */
+// Constants for password reset and error messages
+const TOKEN_BLACKLISTED_ERROR_MESSAGE = "Invalid or expired token";
+const INVALID_PASSWORD_OR_TOKEN_ERROR_MESSAGE = "Invalid password or token";
+const PASSWORD_CHANGE_SUCCESS_MESSAGE = "Password changed";
+const PASSWORD_CHANGE_FAILURE_MESSAGE = "Current password is incorrect";
+const PASSWORD_RESET_SUCCESS_MESSAGE = "Password reset link sent to email";
+const PASSWORD_RESET_FAILURE_MESSAGE = "Failed to send email";
+const USER_NOT_FOUND_ERROR_MESSAGE = "User not found";
+const INVALID_EMAIL_ERROR_MESSAGE = "Email not registered";
+const PASSWORD_UPDATE_NOTIFICATION_MESSAGE = "Password changed";
+
+// Default URL host for generating reset links.
 const HOST = process.env.HOST || 'localhost';
 
-/**
- * Default port for generating reset links.
- * @constant {string}
- */
+// Default port for generating reset links.
 const PORT = process.env.PORT || '3000';
 
-/**
- * Default reset token expiration time (30 minutes).
- * @constant {number}
- */
+// Default reset token expiration time (30 minutes).
 const RESET_TOKEN_EXPIRATION = 30 * 60 * 1000;
 
-/**
- * Default number of allowed notifications (15).
- * @constant {number}
- */
+// Default number of allowed notifications (15).
 const MAX_NOTIFICATIONS = 15;
 
-/**
- * Generate a reset token using uuid.v4.
- * @returns {string} A unique uuid.
- */
+// Generate a reset token using uuid.v4.
 function resetToken() {
-  return v4();
+ return uuidv4();
 }
 
-/**
- * Send reset link password to users.
- * @param {Object} req - Express Request
- * @param {Object} res - Express Response
- * @return {void}
- */
+// Send reset link password to users.
 export async function forgotPass(req, res) {
-  try {
-    // validate request
+ try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return handleResponse(res, 400, errors.array()[0].msg);
@@ -64,7 +55,7 @@ export async function forgotPass(req, res) {
     const user = await User.findOne({ email: email });
 
     if (!user) {
-      return handleResponse(res, 404, `${email} not registered`);
+      return handleResponse(res, 404, INVALID_EMAIL_ERROR_MESSAGE);
     }
 
     const token = resetToken();
@@ -86,29 +77,24 @@ export async function forgotPass(req, res) {
     sender.sendMail(receiver, (error, info) => {
       if (error) {
         logger.error('Failed to send email');
-        return handleResponse(res, 500, 'Failed to send email', error);
+        return handleResponse(res, 500, PASSWORD_RESET_FAILURE_MESSAGE, error);
       }
-      return handleResponse(res, 201, 'Password reset link sent to email');
+      return handleResponse(res, 201, PASSWORD_RESET_SUCCESS_MESSAGE);
     });
-  } catch (error) {
+ } catch (error) {
     return handleResponse(res, 500, 'Internal Server Error', error);
-  }
+ }
 }
 
-/**
- * Validate reset token.
- * @param {Object} req - Express Request
- * @param {Object} res - Express Response
- * @return {void}
- */
+// Validate reset token.
 export async function VerifyResetPass(req, res) {
-  try {
+ try {
     const { token } = req.params;
 
-    if (!token) return handleResponse(res, 401, 'Requires a token');
+    if (!token) return handleResponse(res, 401, TOKEN_BLACKLISTED_ERROR_MESSAGE);
 
     if (isTokenBlacklisted(token)) {
-      return handleResponse(res, 401, 'Invalid or expired token');
+      return handleResponse(res, 401, TOKEN_BLACKLISTED_ERROR_MESSAGE);
     }
 
     const user = await User.findOne({
@@ -117,27 +103,21 @@ export async function VerifyResetPass(req, res) {
     });
 
     if (!user) {
-      return handleResponse(res, 401, 'Invalid or expired token');
+      return handleResponse(res, 401, TOKEN_BLACKLISTED_ERROR_MESSAGE);
     }
 
     return res.status(200).json({
       message: "success",
       token
     });
-  } catch (error) {
+ } catch (error) {
     return handleResponse(res, 500, 'Internal server error', error);
-  }
+ }
 }
 
-/**
- * Reset user's password.
- * @param {Object} req - Express Request
- * @param {Object} res - Express Response
- * @return {void}
- */
+// Reset user's password.
 export async function ResetPass(req, res) {
-  try {
-    // validate request
+ try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return handleResponse(res, 400, errors.array()[0].msg);
@@ -147,11 +127,11 @@ export async function ResetPass(req, res) {
     const { password } = req.body;
 
     if (!token || !password) {
-      return handleResponse(res, 401, 'Invalid password or token');
+      return handleResponse(res, 401, INVALID_PASSWORD_OR_TOKEN_ERROR_MESSAGE);
     }
 
     if (isTokenBlacklisted(token)) {
-      return handleResponse(res, 401, 'Invalid or expired token');
+      return handleResponse(res, 401, TOKEN_BLACKLISTED_ERROR_MESSAGE);
     }
 
     const user = await User.findOne({
@@ -160,33 +140,26 @@ export async function ResetPass(req, res) {
     });
 
     if (!user) {
-      return handleResponse(res, 401, 'Invalid or expired token');
+      return handleResponse(res, 401, TOKEN_BLACKLISTED_ERROR_MESSAGE);
     }
 
     const saltRounds = 12;
     const salt = await genSalt(saltRounds);
-    // Hash the password
     const hashedNewPassword = await hash(password, salt);
     user.password = hashedNewPassword;
     await user.save();
 
     updateBlacklist(token);
 
-    return handleResponse(res, 200, "Password changed");
-  } catch (error) {
+    return handleResponse(res, 200, PASSWORD_CHANGE_SUCCESS_MESSAGE);
+ } catch (error) {
     return handleResponse(res, 500, 'Internal Server Error', error);
-  }
+ }
 }
 
-/**
- * Change logged-in user password.
- * @param {Object} req - Express Request
- * @param {Object} res - Express Response
- * @return {void}
- */
+// Change logged-in user password.
 export async function changePass(req, res) {
-  try {
-    // validate the request
+ try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return handleResponse(res, 400, errors.array()[0].msg);
@@ -202,13 +175,13 @@ export async function changePass(req, res) {
     const user = await User.findById(userId);
 
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: USER_NOT_FOUND_ERROR_MESSAGE });
     }
 
     const isPasswordValid = await compare(currentPassword, user.password);
 
     if (!isPasswordValid) {
-      return handleResponse(res, 400, 'Current password is incorrect');
+      return handleResponse(res, 400, PASSWORD_CHANGE_FAILURE_MESSAGE);
     }
 
     const saltRounds = 12;
@@ -218,7 +191,7 @@ export async function changePass(req, res) {
     user.password = hashedNewPassword;
 
     // create notification
-    const notify = notifications.generateNotification(user, 'updatedUser', 'Password changed');
+    const notify = notifications.generateNotification(user, 'updatedUser', PASSWORD_UPDATE_NOTIFICATION_MESSAGE);
 
     // Add new notification
     user.notificationsList.push(notify);
@@ -228,8 +201,8 @@ export async function changePass(req, res) {
 
     await user.save();
 
-    return res.status(204).send('Password changed');
-  } catch (error) {
+    return res.status(204).send(PASSWORD_CHANGE_SUCCESS_MESSAGE);
+ } catch (error) {
     return handleResponse(res, 500, 'Internal Server Error', error);
-  }
+ }
 }
